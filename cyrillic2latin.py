c9de41b448c801a500f880518939a29704b18d6d
Daniel <wodemoneke@gmail.com>
2008-12-22 23:37:31 +0200

    reached special cases

--- cyrillic2latin.py
+++ cyrillic2latin.py
@@ -13,6 +13,11 @@ class CyrillicLatin:
         self.delete_startpoint = []
         self.ignore_endpoint = []
         self.delete_endpoint = []
+        self.hold_chars = ['s', 'y', 'c']
+        self.hold_chars2 = ['sh', 'ya', 'yo']
+        self.special_cases = {'sh' : u'ш', 'sht' : u'щ', 'you' : u'ю', 'ya' : u'я', 'ch' : u'ч'}
+        
+        
         
         if latin:
             self.convert_to_cyrillic()
@@ -28,17 +33,45 @@ class CyrillicLatin:
         self.delete_startpoint.append(start)
         self.delete_endpoint.append(end)
   
+    def check_holding_chars(current_char, temp_chars):
+        '''check if the current char and temp chars can be conecated into a longer string
+        if so: return the longer string, and undo the holding situation
+        if not: append the '''
+        if temp_chars == '':
+            return current_char
+        x = temp_chars + current_char
+        if x in self.hold_chars2:
+            return temp_chars + current_char
+        else:
+            #if the previous statements weren't true, we can assume that the
+            #temp string is in its final form, perhaps the current_char
+            #will be seperated, and may even makeup a new holdingchar
+            if x in self.special_cases:
+                
+        
   
-    def should_we_hold(self):
-        self.hold_chars = ['s', 'y', 'c']
-        self.hold_chars2 = ['sh', 'ya', 'yo']
-        self.special_cases = {'sh' : u'ш', 'sht' : u'щ', 'you' : u'ю', 'ya' : u'я', 'ch' : u'ч'}
-        
-        
     def convert_to_cyrillic(self, file_name = None):
+        self.hold = False
+        self.new_string = []
+        temp_chars = ''
         for ch in self.mangled_string:
-            index = self.latin_chars.index(ch)
-            print self.cyrillic_chars[index]
+            try:
+                index = self.latin_chars.index(ch)
+                if ch in self.hold_chars:
+                    self.hold = True
+                    
+                if self.hold is True:
+                    temp_chars = check_holding_chars(ch, temp_chars)
+                else:
+                    self.new_string.append(self.cyrillic_chars[index])
+                    
+                    
+            except ValueError:
+                #the letter isnt in the list of latin letters, so just print as is.
+                #to start with we will print ? until bugs are ironed out
+                
+                #self.new_string.append(self.latin_chars[index])
+                self.new_string.append('?')
 
 if __name__ == '__main__':
     convert_string = """momicheta shte beshe momicheta"""
