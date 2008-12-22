5b2f0c309097bf6b63736923e7987fcee5752d9d
Daniel <wodemoneke@gmail.com>
2008-12-22 16:57:33 +0200

    created cyrillic2latin transliterator

--- cyrillic2latin.py
+++ cyrillic2latin.py
@@ -8,9 +8,6 @@ class CyrillicLatin:
     def __init__(self, mangled_string, latin = True):
         self.latin_chars = u'a b c d e f g h i j k l m n o p q r s t u v x z 6 4'.split()
         self.cyrillic_chars = u'а б с д е ф г х и ж к л м н о п я р с т у в я в х з б ч'.split()
-        self.hold_chars = ['s', 'y', 'c']
-        self.hold_chars2 = ['sh', 'ya', 'yo']
-        self.special_cases = {'sh' : u'ш', 'sht' : u'щ', 'you' : u'ю', 'ya' : u'я', 'ch' : u'ч'}
         self.mangled_string = unicode(string.lower(mangled_string))
         self.ignore_startpoint = []
         self.delete_startpoint = []
@@ -31,17 +28,24 @@ class CyrillicLatin:
         self.delete_startpoint.append(start)
         self.delete_endpoint.append(end)
   
+  
+    def should_we_hold(self):
+        self.hold_chars = ['s', 'y', 'c']
+        self.hold_chars2 = ['sh', 'ya', 'yo']
+        self.special_cases = {'sh' : u'ш', 'sht' : u'щ', 'you' : u'ю', 'ya' : u'я', 'ch' : u'ч'}
+        
+        
     def convert_to_cyrillic(self, file_name = None):
-        new_string = []
+        self.self.new_string = []
         previous_chars = []
         ignore = False
         delete = False
-        hold_chars = []
-        hold = False
+        self.hold_chars = []
+        self.hold = False
         for ch in self.mangled_string:            
             if ch in self.ignore_startpoint:
                 ignore = True
-                new_string.append(ch)
+                self.new_string.append(ch)
                 continue
                 
             if ch in self.delete_startpoint:
@@ -50,7 +54,7 @@ class CyrillicLatin:
             
             if ch in self.ignore_endpoint:
                 ignore = False
-                new_string.append(ch)
+                self.new_string.append(ch)
                 continue
             
             if ch in self.delete_endpoint:
@@ -58,7 +62,7 @@ class CyrillicLatin:
                 continue
             
             if ignore is True:
-                new_string.append(ch)
+                self.new_string.append(ch)
                 continue
             
             if delete is True:
@@ -66,25 +70,23 @@ class CyrillicLatin:
             
             
             if ch in string.whitespace:
-                new_string.append(ch)
+                self.new_string.append(ch)
             else:
                 try:
                     i = self.latin_chars.index(ch)
-                    if hold is True:
-                        hold_chars.append(ch)
-                        if hold_chars in 
-                    new_string.append(self.cyrillic_chars[i])
+                    self.should_we_hold(ch)
+                    self.new_string.append(self.cyrillic_chars[i])
                     previous_chars.append(ch)
                 except:
-                    new_string.append('?')
-        new_string = unicode(''.join(new_string))
+                    self.new_string.append('?')
+        self.new_string = unicode(''.join(self.new_string))
         
         if file_name is None:
-            return new_string
+            return self.new_string
         
         else:
             output = open(file_name, 'w')
-            output.write(new_string)
+            output.write(self.new_string)
             
             
 
